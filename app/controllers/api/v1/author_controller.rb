062cc567901c807251936ec646b715dfe5e79f10
chotung <38818833+chotung@users.noreply.github.com>
2021-02-24 01:24:42 -0500

    fixed controller and setup proxy

--- app/controllers/api/v1/author_controller.rb
+++ app/controllers/api/v1/author_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::AuthorController < ApplicationController
+		before_action :find_author, only: [:show, :update, :destory]
+	def index
+		@authors = Author.all
+		render json: @authors
+	end
+
+	def show
+		render json: @author
+	end
+
+	def create
+		@author = Author.new(author_params)
+		if @author.save
+			render json: @author
+		else
+			render error: { error: 'Unable to create author.' }, status: 400
+		end
+	end
+	
+	def update
+		if @author
+			@author.update(author_params)
+			render json: { message: 'author successfully updated.' }, status: 200
+		else
+			render json: { error: 'Unable to update author.' }, status:400
+		end
+	end
+
+	def destory
+		if @author
+			@author.destory
+			render json: { message: 'author successfully deleted.' }, status: 200
+		else
+			render json: { error: 'Unable to delete author.' }, status:400
+		end
+	end
+
+	private
+	def author_params
+		params.require(:author).permit(:name)
+	end
+	# exmaple of DRYing up code
+	def find_author
+		@author = Author.find(params[:id])
+	end
 end
--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::BookController < ApplicationController
+	before_action :find_book, only: [:show, :update, :destory]
+	def index
+		@books = Book.all
+		render json: @books
+	end
+
+	def show
+		render json: @book
+	end
+
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			render json: @book
+		else
+			render error: { error: 'Unable to create book.' }, status: 400
+		end
+	end
+	
+	def update
+		if @book
+			@book.update(book_params)
+			render json: { message: 'book successfully updated.' }, status: 200
+		else
+			render json: { error: 'Unable to update book.' }, status:400
+		end
+	end
+
+	def destory
+		if @book
+			@book.destory
+			render json: { message: 'book successfully deleted.' }, status: 200
+		else
+			render json: { error: 'Unable to delete book.' }, status:400
+		end
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:name)
+	end
+	# exmaple of DRYing up code
+	def find_book
+		@book = Book.find(params[:id])
+	end
 end
